ade2974b16080e87d5a61ad67fde0addd464d1e2
Ina <ina.flaryanovich@gmail.com>
2024-11-22 20:43:38 +0100

    fix: .env file issue resolving

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
